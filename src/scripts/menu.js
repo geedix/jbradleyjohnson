e4d0a2e5eb24a78724126361bafc3bc30895bf61
geedix <bradjohnson@posteo.net>
2025-12-19 15:49:12 -0500

    nav fix

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,4 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
-    console.log("toggled menu");
+    console.log("toggled menu "+document.querySelector('.nav-links').classList);
 });
