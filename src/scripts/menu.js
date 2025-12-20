aff0489a8f48d59a2d510637d977624ab9406af8
geedix <bradjohnson@posteo.net>
2025-12-20 11:24:28 -0500

    nav etc

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,4 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
-    console.log("toggled menu "+document.querySelector('.nav-links').classList);
+    // document.querySelector('.nav-links').classList.toggle('expanded');
+    console.log("toggled menu in menu.js: "+document.querySelector('.nav-links').classList);
 });
