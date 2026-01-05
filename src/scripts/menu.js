f0520ab0b7200ce36aa9ed1dcd5000bcd5257459
geedix <bradjohnson@posteo.net>
2026-01-05 15:29:23 -0500

    added hero to work page

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,5 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
     // document.querySelector('.nav-links').classList.toggle('expanded');
+    // this now happens in the Navigation component
     console.log("toggled menu in menu.js: "+document.querySelector('.nav-links').classList);
 });
